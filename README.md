f05afe8d2d2ec9b60337c8d1763eaf583878237c
ivomarsan <ivomarlucio@gmail.com>
2017-11-30 17:10:37 -0300

    End of Day

--- README.md
+++ README.md
@@ -4,6 +4,207 @@
   </a>
 </p>
 
-<p align="center">i-select</p>
+<p align="center">i-dropdown</p>
 
-# Working...
+<p align="center">
+
+  <a href="https://www.npmjs.com/package/i-dropdown">
+    <img src="https://img.shields.io/npm/dt/i-dropdown.svg" alt="Downloads">
+  </a>
+
+  <a href="https://www.npmjs.com/package/i-dropdown">
+    <img src="https://img.shields.io/npm/v/i-dropdown.svg" alt="Version">
+  </a>
+
+  <a href="https://www.npmjs.com/package/i-dropdown">
+    <img src="https://img.shields.io/npm/l/i-dropdown.svg" alt="License">
+  </a>
+</p>
+
+---
+
+<a href="https://www.npmjs.com/package/i-dropdown">`i-dropdown`</a> is a
+simplistic button forked from
+<a href="https://www.npmjs.com/package/vue-material">`vue-material`</a> inspired
+in <a href="http://material.google.com" target="_blank">Material Design</a>
+specs.
+
+## Installation
+
+Install via `npm`
+
+```bash
+npm install i-dropdown --save
+```
+
+Import or require in your code:
+
+```javascript
+import Vue from 'vue';
+import iButton from 'i-dropdown';
+
+// OR
+
+var Vue = require('vue');
+var iButton = require('i-dropdown');
+```
+
+## Installation
+
+### Module
+
+```javascript
+import iButton from 'i-dropdown';
+
+// ...
+
+export default {
+  // ...
+  components: {
+    'my-awesome-button': iButton,
+  },
+  // ...
+};
+```
+
+### Browser
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="dist/i-dropdown.min.js"></script>
+<script>
+Vue.use(iButton);
+
+new Vue({
+  el: '#app'
+});
+</script>
+```
+
+## Usage
+
+It's very useful to use `i-dropdown` you only need to register then :smile:
+seems like with
+
+```html
+<i-dropdown class="is-background-red">
+  🗑
+</i-dropdown>
+```
+
+You also can use some properties like
+
+#### A Link _(href & target & rel)_
+
+```html
+<i-dropdown class="is-background-red"
+:href="'http://ivomarsan.com'"
+:target="'_blank'"
+:rel="'noopener'">
+  🗑
+</i-dropdown>
+```
+
+#### Button _(disabled & type)_
+
+```html
+<i-dropdown class="is-background-red"
+:disabled="isDisabled"
+:type="'button'">
+  🗑
+</i-dropdown>
+```
+
+#### With a Tooltip
+
+```html
+<i-dropdown class="is-outline-blue"
+tooltip="This is a Tooltip"
+tooltip-pos="top">
+  🗑
+</i-dropdown>
+```
+
+This example will open a `Tooltip` with a message to `top`. See above all
+tooltips positions (`toolpos`) avaliable:
+
+* `top`
+* `left`
+* `right`
+* `bottom`
+
+`toolpos` its a _String_ and has by default value: `top`
+
+## Personalization
+
+You can personalize your `<i-dropdown>` with class `i-dropdown` or pre-defined
+classes to change background, color or outline (_list bellow_) or through some
+properties.
+
+`uppercase` (_Boolean_) if true, text will be UPPERCASED.
+
+```html
+<i-dropdown uppercase>🗑</i-dropdown>
+```
+
+`is-color` (_String: Hex_) to define a color to button.
+
+```html
+<i-dropdown is-color="#d43f3a">🗑</i-dropdown>
+```
+
+`is-outline` (_String: Hex_) to draw a custom border.
+
+```html
+<i-dropdown is-outline="#d43f3a">🗑</i-dropdown>
+```
+
+`is-background` (_String: Hex_) to get a background color.
+
+```html
+<i-dropdown is-background="#d43f3a">🗑</i-dropdown>
+```
+
+### Colors
+
+Actually we have 10 colors, are:
+
+* <img src="https://img.shields.io/badge/red-                    -d43f3a.svg?style=for-the-badge" alt="red">
+* <img src="https://img.shields.io/badge/pink-                    -ff067c.svg?style=for-the-badge" alt="pink">
+* <img src="https://img.shields.io/badge/blue-                    -0488bb.svg?style=for-the-badge" alt="blue">
+* <img src="https://img.shields.io/badge/gray-                    -ada8a5.svg?style=for-the-badge" alt="gray">
+* <img src="https://img.shields.io/badge/black-                    -000000.svg?style=for-the-badge" alt="black">
+* <img src="https://img.shields.io/badge/white-                    -ffffff.svg?style=for-the-badge" alt="white">
+* <img src="https://img.shields.io/badge/green-                    -4cae4c.svg?style=for-the-badge" alt="green">
+* <img src="https://img.shields.io/badge/purple-                    -9400c8.svg?style=for-the-badge" alt="purple">
+* <img src="https://img.shields.io/badge/yellow-                    -ffdf00.svg?style=for-the-badge" alt="yellow">
+* <img src="https://img.shields.io/badge/orange-                    -ff9e00.svg?style=for-the-badge" alt="orange">
+
+### Classes
+
+Classes are formed by `prefix-type-color`, examples:
+
+`.is-color-green`
+
+```html
+<i-dropdown class="is-color-green">🗑</i-dropdown>
+```
+
+`.is-outline-green`
+
+```html
+<i-dropdown class="is-outline-green">🗑</i-dropdown>
+```
+
+`.is-background-green`
+
+```html
+<i-dropdown class="is-background-green">🗑</i-dropdown>
+```
+
+> See more in [i-colors](https://www.npmjs.com/package/i-colors) and examples.
+> It's easier :smile:
+
+## Demo
+
+[JSFiddle](https://jsfiddle.net/r5yf1qtv/)
--- src/iDropdown.vue
+++ src/iDropdown.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="dropdown" role="button" @mousedown.prevent="toggleDropdown">
-    <input value="1" type="hidden" v-model="model">
-    <span ref="selected">
-      {{ model }}
-    </span>
-    <i ref="dropdownIcon" role="presentation" class="open-indicator">
-      
-    </i>
+  <div class="dropdown">
+    <div ref="toggle" role="button" @mousedown.prevent="openDropdown" :class="{inline}">
+      <input value="1" type="hidden" v-model="model">
+      <span ref="selected">
+        {{ getLabel(mutableValue) }}
+      </span>
 
+      <slot name="icon">
+        <i role="presentation" class="i-arrow_drop_down"></i>
+      </slot>
+    </div>
     <!-- <input ref="search" v-model="search" @keydown.delete="maybeDeleteValue" @keyup.esc="onEscape" @keydown.up.prevent="typeAheadUp"
         @keydown.down.prevent="typeAheadDown" @keydown.enter.prevent="typeAheadSelect" @blur="onSearchBlur" @focus="onSearchFocus"
         type="search" class="form-control" :disabled="disabled" :placeholder="searchPlaceholder" :tabindex="tabindex" :readonly="!searchable"
@@ -18,20 +20,30 @@
       </slot> -->
 
 
-    <transition>
-      <ul v-if="dropdownOpen" class="dropdown-menu" :style="{ 'max-height': maxHeight }">
-        <li v-for="(option, index) in filteredOptions" v-bind:key="index" :class="{ active: isOptionSelected(option), highlight: index === typeAheadPointer }"
-          @mouseover="typeAheadPointer = index">
-          <a @mousedown.prevent="select(option)">
-            <slot name="option" v-bind="option">
-              {{ getOptionLabel(option) }}
-            </slot>
-          </a>
-        </li>
-        <li v-if="!filteredOptions.length" class="no-options">
-          <slot name="no-options">Sorry, no matching options.</slot>
-        </li>
-      </ul>
+    <transition name="fade">
+      <section v-show="dropdownOpen">
+        <!-- <input ref="search" v-model="search" @keydown.delete="maybeDeleteValue" @keyup.esc="onEscape" @keydown.up.prevent="typeAheadUp"
+          @keydown.down.prevent="typeAheadDown" @keydown.enter.prevent="typeAheadSelect" @blur="onSearchBlur" @focus="onSearchFocus"
+          type="search" class="form-control" :disabled="disabled" :placeholder="searchPlaceholder" :tabindex="tabindex" :readonly="!searchable"
+          :style="{ width: isValueEmpty ? '100%' : 'auto' }" :id="inputId" aria-label="Search for option"> -->
+        <input v-if="filter" ref="search" v-model="search" :placeholder="placeholder">
+
+        <ul :style="{ 'max-height': maxHeight, 'margin': hideResults || !filter ? '0' : '0 0 0.75rem' }">
+          <li v-for="(option, index) in filteredOptions" :key="index" :class="{ selected: isOptionSelected(option) }">
+            <a @mousedown.prevent="select(option)">
+              <slot name="option" v-bind="option">
+                {{ getLabel(option) }}
+              </slot>
+            </a>
+          </li>
+          <li v-if="!filteredOptions.length" class="no-options">
+            <slot name="no-options">{{ noMatching }}</slot>
+          </li>
+        </ul>
+        <slot name="total-results" v-if="filter && !hideResults && filteredOptions.length < mutableOptions.length">
+          <span class="total-results">Showing {{ filteredOptions.length }} from {{ mutableOptions.length }} results</span>
+        </slot>
+      </section>
     </transition>
     <!-- <div class="menu ui transition" :class="{hidden: !showDropdown}">
       <div class="item active" data-value="1">
@@ -52,67 +64,215 @@
 
 <script>
 export default {
-  name: 'md-button',
+  name: 'i-dropdown',
   components: {},
   data: () => ({
+    search: '',
     dropdownOpen: false,
   }),
+
+  /**
+   * Clone props into mutable values,
+   * attach any event listeners.
+   */
+  created() {
+    this.mutableValue = this.value || this.initial;
+    this.mutableOptions = this.options.slice(0);
+  },
+  watch: {
+    /**
+     * When the value prop changes, update
+     * the internal mutableValue.
+     * @param  {mixed} val
+     * @return {void}
+     */
+    value(val) {
+      this.mutableValue = val;
+    },
+  },
   props: {
     /**
-       * Contains the currently selected value. Very similar to a
-       * `value` attribute on an <input>. You can listen for changes
-       * using 'change' event using v-on
-       * @type {Object||String||null}
-       */
+     * Contains the currently selected value. Very similar to a
+     * `value` attribute on an <input>.
+     * @type {Object||String||null}
+     */
     value: {
       default: null,
     },
+
     /**
-       * An optional callback function that is called each time the selected
-       * value(s) change. When integrating with Vuex, use this callback to trigger
-       * an action, rather than using :value.sync to retreive the selected value.
-       * @type {Function}
-       * @param {Object || String} val
-       */
-    onChange: {
-      type: Function,
-      default: function(val) {
-        this.$emit('input', val);
+     * Initial value showing on `i-dropdown`
+     * @type {String}
+     */
+    initial: {
+      type: String,
+      default: 'i-dropdown',
+    },
+
+    /**
+     * Equivalent to the `placeholder` attribute on an `<input>`.
+     * Used in filter
+     * @type {String}
+     */
+    placeholder: {
+      type: String,
+      default: 'Type your query',
+    },
+
+    /**
+     * Alternative message to show when has no matching on filter
+     * @type {String}
+     */
+    noMatching: {
+      type: String,
+      default: 'Sorry, no matching options.',
+    },
+
+    /**
+     * An array of strings or objects to be used as dropdown choices.
+     * If you are using an array of objects, vue-select will look for
+     * a `label` key (ex. [{label: 'This is Foo', value: 'foo'}]). A
+     * custom label key can be set with the `label` prop.
+     * @type {Array}
+     */
+    options: {
+      type: Array,
+      default() {
+        return [];
       },
     },
+
     /**
-       * Sets the max-height property on the dropdown list.
-       * @deprecated
-       * @type {String}
-       */
+     * Tells vue-select what key to use when generating option
+     * labels when each `option` is an object.
+     * @type {String}
+     */
+    label: {
+      type: String,
+      default: 'label',
+    },
+
+    /**
+     * Limit how much results will be avaliable in Dropdown
+     * @type {String}
+     */
+    limit: {
+      type: String,
+      default: '5',
+    },
+
+    /**
+     * When True a input search will be avaliable
+     * @type {Boolean}
+     */
+    filter: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Disable the entire component.
+     * @type {Boolean}
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Hide results from Dropdown
+     * when filteredOptions is called
+     * @type {Boolean}
+     */
+    hideResults: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Display element inline
+     * its mean, no borders
+     * @type {Boolean}
+     */
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Sets the max-height property on the dropdown list.
+     * @deprecated
+     * @type {String}
+     */
     maxHeight: {
       type: String,
-      default: '400px',
+      default: '200px',
     },
   },
   computed: {
+    /**
+     * Return Selected value to v-model.
+     * @return {Object|String}
+     */
     model: {
       get() {
-        return this.value;
+        return this.mutableValue;
       },
       set(v) {
         this.$emit('input', v);
       },
     },
+
+    /**
+     * The currently displayed options, filtered
+     * by the search elements value. If tagging
+     * true, the search text will be prepended
+     * if it doesn't already exist.
+     *
+     * @return {array}
+     */
+    filteredOptions() {
+      let options = this.mutableOptions.filter(option => {
+        if (typeof option === 'object' && option.hasOwnProperty(this.label)) {
+          return (
+            option[this.label]
+              .toString()
+              .toLowerCase()
+              .indexOf(this.search.toLowerCase()) > -1
+          );
+        } else if (
+          typeof option === 'object' &&
+          !option.hasOwnProperty(this.label)
+        ) {
+          return console.warn(
+            `[vue-select warn]: Label key "option.${this
+              .label}" does not exist in options object.\nhttp://sagalbot.github.io/vue-select/#ex-labels`,
+          );
+        }
+        return option.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+      });
+      return options.slice(0, +this.limit);
+    },
   },
   methods: {
     /**
-       * Toggle the visibility of the dropdown menu.
-       * @param  {Event} e
-       * @return {void}
-       */
+     * Toggle the visibility of the dropdown menu.
+     * @param  {Event} e
+     * @return {void}
+     */
     toggleDropdown(e) {
       if (
-        e.target === this.$refs.dropdownIcon ||
-        // e.target === this.$refs.search ||
-        e.target === this.$refs.selected ||
-        e.target === this.$el
+        // e.target === this.$refs.selected ||
+        e.target === this.$refs.toggle
+        // e.target === this.$refs.icon ||
+        // e.target === this.$el
       ) {
+        this.dropdownOpen = !this.dropdownOpen;
+        console.log(this.$refs.search);
+        if (this.$refs.search) {
+          this.$refs.search.focus();
+        }
+
         if (this.open) {
           // this.$refs.search.blur(); // dropdown will close on blur
         } else {
@@ -123,17 +283,53 @@ export default {
         }
       }
     },
-    toggle() {
-      this.dropdownOpen = !this.dropdownOpen;
+
+    /**
+     * Open Dropdown
+     * @param  {Event} e
+     * @return {void}
+     */
+    openDropdown(e) {
+      this.search && (this.search = '');
+      this.$refs.search && this.$refs.search.focus();
+
+      this.dropdownOpen = true;
     },
 
     /**
-       * Select a given option.
-       * @param  {Object|String} option
-       * @return {void}
-       */
+     * Close Dropdown
+     * @param  {Event} e
+     * @return {void}
+     */
+    closeDropdown() {
+      this.dropdownOpen = false;
+    },
+
+    /**
+     * Select a given option.
+     * @param  {Object|String} option
+     * @return {void}
+     */
     select(option) {
       this.model = option;
+      this.closeDropdown();
+    },
+
+    /**
+     * Callback to generate the label text. If {option}
+     * is an object, returns option[this.label] by default.
+     * @type {String}
+     * @param  {Object || String} option
+     * @return {String}
+     */
+    getLabel(option) {
+      if (typeof option === 'object' && this.label && !!option[this.label])
+        return option[this.label];
+      return option;
+    },
+
+    isOptionSelected(option) {
+      return this.mutableValue === option;
     },
   },
 };
